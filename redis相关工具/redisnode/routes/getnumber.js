2abfde707386cad0a2eb5152807b69d271c9f63d
gotodk <gotodk@sina.com>
2017-08-06 15:32:49 +0800

    初始备份

---
+++ redis相关工具/redisnode/routes/getnumber.js
@@ -0,0 +1,65 @@
+﻿var express = require('express');
+var router = express.Router();
+const Redis = require('ioredis');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+
+
+    const config = {
+        port: 16379,          // Redis port
+        host: 'home.gotodk.cn',   // Redis host
+        password: 'tudoudigua',
+        family: 4,           // 4 (IPv4) or 6 (IPv6)
+        db: 0
+    };
+
+
+    //当前日期
+    var date = new Date();
+    var year = date.getFullYear();
+    year = year.toString().substr(2, 2);
+    var month = date.getMonth() + 1;
+    month = (Array(2).join(0) + month).slice(-2);
+    var day = date.getDate();
+    day = (Array(2).join(0) + day).slice(-2);
+    var hour = date.getHours();
+    hour = (Array(2).join(0) + hour).slice(-2);
+    var minute = date.getMinutes();
+    minute = (Array(2).join(0) + minute).slice(-2);
+    var second = date.getSeconds();
+    second = (Array(2).join(0) + second).slice(-2);
+    //获得日期前缀(精确到分钟或秒从这里加上)
+    var date_str = year + '' + month + '' + day + '';
+
+
+    if（是否可以用）
+    锁定（）
+
+    let redis = new Redis(config);
+    //设置24小时候后过期
+    redis.expire('MyNumber' + date_str, 60*60*24);
+    //累加编码并立刻获取
+    redis.hincrby('MyNumber' + date_str, req.query.modname, 1).then(rst => {
+       
+        //console.log(rst);
+        //顺序号补几个零，超过补位的数不会截断
+        var len = 5;
+        if ((rst + '').length < len) {
+            rst = (Array(len).join(0) + rst).slice(-len);
+        }
+        var newnumber_str = date_str+''+rst+'';
+        //输出
+        res.render('getnumber', { newnumber: newnumber_str });
+
+    });
+
+
+解锁（）
+
+
+
+
+});
+
+module.exports = router;
---
+++ redis相关工具/redisnode/routes/index.js
@@ -0,0 +1,33 @@
+﻿var express = require('express');
+var router = express.Router();
+const Redis = require('ioredis');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  
+
+    const config = {
+        port: 16379,          // Redis port
+        host: 'home.gotodk.cn',   // Redis host
+        password: 'tudoudigua',
+        family: 4,           // 4 (IPv4) or 6 (IPv6)
+        db: 0
+    };
+
+  let redis = new Redis(config);
+
+  redis.sadd('userA', ['孙悟空', '猪八戒', '狮子头', '飞机', '大炮']);
+  redis.sadd('userB', ['流川枫', '电脑', '孙悟空', '天使', '飞机']);
+  redis.sadd('userC', ['飞机', '溜溜球', '孙悟空']);
+  //获得三个集合的交集
+  redis.sinter('userA', 'userB', 'userC').then(rst => {
+      //console.log(rst);
+      res.render('index', { title: '集合例子', pubkeystr: rst });
+  });
+  
+
+
+
+});
+
+module.exports = router;
